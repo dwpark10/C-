6b7b41ec5e431cae96a2552cbf3f4e8664b8b803
DaeWon Park <dwpark10@dwpark10.local>
2018-03-28 22:51:55 +0900

    new commit

---
+++ BAEKJOON/Full_Search/etc/2003.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int s;
+	cin >> n >> s;
+	vector<int> a(n);
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	int left = 0;
+	int right = 0;
+	int sum = a[0];
+	int ans = 0;
+	while( left <= right && right < n ){
+		if(sum < s){
+			right += 1;
+			sum += a[right];
+		}else if(sum == s){
+			ans += 1;
+			right += 1;
+			sum += a[right];
+		}else if(sum < s){
+			sum -= a[left];
+			left += 1;
+			if(left > right && left < n){
+				right = left;
+				sum = a[left];
+			}
+		}
+	}
+	cout << sum << '\n';
+	return 0;
+}
