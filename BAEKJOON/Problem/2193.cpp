48a1e7b6fe4d14d972b56215ce08b24a31f3d784
daewon10.park <daewon10.park@lge.com>
2018-04-10 10:31:58 +0900

    2193.cpp
    Change-Id: Ieb42eaf4f7613ab47c7531662dd6a48515e975f5

---
+++ BAEKJOON/Problem/2193.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[2][101];
+
+int main(){
+	arr[0][0] = 0;
+	arr[1][0] = 0;
+
+	arr[0][1] = 0;
+	arr[1][1] = 1;
+
+	arr[0][2] = 1;
+	arr[1][2] = 0;
+
+	for(int i = 3; i <= 100; i++){
+		arr[0][i] = arr[0][i-1] + arr[1][i-1];
+		arr[1][i] = arr[0][i-1];
+	}
+
+	int N;
+
+	cin >> N;
+
+	cout << arr[0][N] + arr[1][N] << '\n';
+
+	return 0;
+}
